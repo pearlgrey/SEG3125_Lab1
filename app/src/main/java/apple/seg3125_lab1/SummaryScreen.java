760df2e2da62659ad608158045638b3eff4524c2
ruic3429 <ruic3429@gmail.com>
2015-05-21 15:26:33 -0400

    space

--- app/src/main/java/apple/seg3125_lab1/SummaryScreen.java
+++ app/src/main/java/apple/seg3125_lab1/SummaryScreen.java
@@ -10,7 +10,7 @@ import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 
-//SummaryScreen 
+//SummaryScreen    
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
